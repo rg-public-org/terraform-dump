470e148a90c4b9a44807a9cbffe7359cc4f79d93
Ruben <66105177+rubengm13@users.noreply.github.com>
2024-09-13 10:43:19 -0700

    Create pods.tf

---
+++ kubernetes/pods.tf
@@ -0,0 +1,21 @@
+resource "kubernetes_pod" "test" {
+  metadata {
+    name = "terraform-example"
+  }
+
+  spec {
+    container {
+      image = "nginx:1.21.6"
+      name  = "example"
+
+      env {
+        name  = "environment"
+        value = "test"
+      }
+
+      port {
+        container_port = 80
+      }
+    }
+  }
+}
